fca9bc1937f812e63f1720535b5ab1cbfb072556
Sekwah <sekwah41@gmail.com>
2020-12-09 23:03:42 +0000

    Adding base project

---
+++ src/main/java/com/sekwah/sekclib/SekCLib.java
@@ -0,0 +1,40 @@
+package com.sekwah.sekclib;
+
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(SekCLib.MOD_ID)
+public class SekCLib {
+
+    public static final String MOD_ID = "sekclib";
+
+    public static final Logger LOGGER = LogManager.getLogger("SekC Lib");
+
+    public SekCLib() {
+
+        ModLoadingContext loadingContext = ModLoadingContext.get();
+
+
+    }
+
+
+    private void clientSetup(final FMLClientSetupEvent event) {
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
+
+    @SubscribeEvent
+    public static void onServerStarting(RegisterCommandsEvent event) {
+
+    }
+
+}

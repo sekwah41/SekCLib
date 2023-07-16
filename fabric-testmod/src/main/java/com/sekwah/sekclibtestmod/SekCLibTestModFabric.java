6eb84d798691b053b5b20fe8edd6ee710cd8f889
Sekwah <contact@sekwah.com>
2023-07-16 20:42:14 +0100

    chore: update test mod

---
+++ fabric-testmod/src/main/java/com/sekwah/sekclibtestmod/SekCLibTestModFabric.java
@@ -0,0 +1,10 @@
+package com.sekwah.sekclibtestmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SekCLibTestModFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
